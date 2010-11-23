e527861c449b296cf81ef36f68635e3cb2fac506
jmf <jmf@localhost.(none)>
2010-11-23 17:36:29 +0100

    zopeskel init

---
+++ setup.py
@@ -0,0 +1,32 @@
+from setuptools import setup, find_packages
+import os
+
+version = '1.09i.1'
+
+setup(name='collective.js.cufon',
+      version=version,
+      description="Cufon aims to become a worthy alternative to sIFR",
+      long_description=open("README.txt").read() + "\n" +
+                       open(os.path.join("docs", "HISTORY.txt")).read(),
+      # Get more strings from
+      # http://pypi.python.org/pypi?%3Aaction=list_classifiers
+      classifiers=[
+        "Programming Language :: Python",
+        ],
+      keywords='',
+      author='',
+      author_email='jeanmichel.francois@makina-corpus.com',
+      url='http://svn.plone.org/svn/collective/',
+      license='GPL',
+      packages=find_packages(exclude=['ez_setup']),
+      namespace_packages=['collective', 'collective.js'],
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=[
+          'setuptools',
+          # -*- Extra requirements: -*-
+      ],
+      entry_points="""
+      # -*- Entry points: -*-
+      """,
+      )
